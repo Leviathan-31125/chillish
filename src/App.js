c5e663b344b96f1028c6ddb69f0765eea8734caa
rayilahude <rayfikar@gmail.com>
2025-07-20 10:42:43 +0700

    FEAT:InitLandingPages

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Chillish
         </a>
       </header>
     </div>
