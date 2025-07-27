ea27a2e79dad71b071835ca95127077683c41395
Leviathan-31125 <athaidar.dev@gmail.com>
2025-07-27 13:51:26 +0700

    Feat: Set up Tailwind & Footer section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import LandingPage from "./Pages/Landing";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Chillish
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<LandingPage />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
