90e1c5981a9069b609470eca3a5f48339223ee39
rayilahude <rayfikar@gmail.com>
2025-10-12 06:38:19 +0700

    pembuatn button hijau dan kuning di jumbotron

--- src/Pages/Landing/index.js
+++ src/Pages/Landing/index.js
@@ -4,10 +4,12 @@ import Product from './ProductSection'
 import Myprofile from './MyProfile/Myprofile'
 import Jumbotron from './Jumbotron'
 import Benefit from './Benefit/Benefit'
+import Header from './Header'
 
 const LandingPage = () => {
   return (
     <div>
+      <Header/>
       <Jumbotron/>
       <Benefit />
       <Myprofile />
